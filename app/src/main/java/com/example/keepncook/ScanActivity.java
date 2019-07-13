c65c27822255d0e02801b189523b4526347d62aa
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-07-13 19:41:07 +0200

    Vision : implement alert dialog to fetch productn name after date scan

--- app/src/main/java/com/example/keepncook/ScanActivity.java
+++ app/src/main/java/com/example/keepncook/ScanActivity.java
@@ -1,15 +1,20 @@
 package com.example.keepncook;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -17,6 +22,11 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Locale;
 
 public class ScanActivity extends AppCompatActivity
 {
@@ -26,6 +36,9 @@ public class ScanActivity extends AppCompatActivity
 
     public static final String SCAN_ACTIVITY = "SCAN_ACTIVITY";
     public static final int requestPermissionID = 101;
+
+    String product_name;
+    Date expiration_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -37,6 +50,45 @@ public class ScanActivity extends AppCompatActivity
         mTextView = findViewById(R.id.text_view);
 
         startCameraSource();
+    }
+
+    public void setProductName(String productName)
+    {
+        this.product_name = productName;
+    }
+
+    public void ConfirmDateAndSaveProduct(View v)
+    {
+        try
+        {
+            this.expiration_date = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE).parse(this.mTextView.getText().toString());
+
+            final EditText productNameEditText = new EditText(this);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Produit")
+                .setMessage(
+                    "Date d'expiration : " + this.expiration_date.toString() +
+                    "\nRenseignez maintenant le nom du produit"
+                )
+                .setView(productNameEditText)
+                .setPositiveButton("Enregistrer", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        setProductName(String.valueOf(productNameEditText.getText()));
+                        // TODO : Implémenter l'enregistrement du produit sur Firebase, Afficher un message de succès et rediriger
+                    }
+                })
+                .setNegativeButton("Annuler", null)
+                .create();
+            dialog.show();
+
+            Log.i(SCAN_ACTIVITY, "Nom du produit : " + this.product_name + ". Date d'expiration : " + this.expiration_date);
+        }
+        catch (ParseException e)
+        {
+            Log.e(SCAN_ACTIVITY, "Parsing error " + e.getMessage());
+            Toast.makeText(this, "Le texte scanné n'est pas une date valide", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void startCameraSource()
